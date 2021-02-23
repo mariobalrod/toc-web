5f216ebb30c21156beb2ff40deb4a57ba2a666c4
mariobalrod <marioballestero.rdg@gmail.com>
2021-02-23 16:19:44 +0100

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAN_w-nyKS6CJtTtO69QGHVaXWvZdpr2Jg",
+    authDomain: "toc-web-b1110.firebaseapp.com",
+    projectId: "toc-web-b1110",
+    storageBucket: "toc-web-b1110.appspot.com",
+    messagingSenderId: "324349253705",
+    appId: "1:324349253705:web:f4406fd1a4ebfd25f5c798",
+    measurementId: "G-FFC2W6EMQF",
+  },
+};
